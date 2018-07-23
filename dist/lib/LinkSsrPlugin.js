71f27002d3fa7e09afaca3fcf4c76f7a2f464172
gaoyuan123 <gaoyuanzlz@qq.com>
2018-07-24 01:09:18 +0800

    fixed bug

--- dist/lib/LinkSsrPlugin.js
+++ dist/lib/LinkSsrPlugin.js
@@ -1,13 +1,4 @@
 "use strict";
-var __rest = (this && this.__rest) || function (s, e) {
-    var t = {};
-    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
-        t[p] = s[p];
-    if (s != null && typeof Object.getOwnPropertySymbols === "function")
-        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) if (e.indexOf(p[i]) < 0)
-            t[p[i]] = s[p[i]];
-    return t;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 class LinkSsrPlugin {
     constructor(options) {
@@ -23,7 +14,7 @@ class LinkSsrPlugin {
             const dllFiles = Object.keys(dllManifest).map(entryName => {
                 return dllManifest[entryName].output;
             });
-            const _a = source.initial, { manifestJs } = _a, otherJs = __rest(_a, ["manifestJs"]);
+            const [manifestJs, ...otherJs] = source.initial;
             source.initial = [manifestJs].concat(dllFiles || []).concat(otherJs);
             source = JSON.stringify(source);
             compilation.assets['vue-ssr-client-manifest.json'] = {
--- src/lib/LinkSsrPlugin.ts
+++ src/lib/LinkSsrPlugin.ts
@@ -21,7 +21,7 @@ export default class LinkSsrPlugin {
             const dllFiles = Object.keys(dllManifest).map(entryName => {
                 return dllManifest[entryName].output;
             })
-            const {manifestJs,...otherJs} = source.initial;
+            const [manifestJs,...otherJs] = source.initial;
             source.initial = [manifestJs].concat(dllFiles || []).concat(otherJs);
 
             source = JSON.stringify(source);
